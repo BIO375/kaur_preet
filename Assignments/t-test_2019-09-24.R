71c3694ea3e7d61bad45b56630b56ae0a1f48495
Manpreet Kaur <manpreetkaur.mk1997@gmail.com>
2019-10-01 02:43:57 -0400

    second attempt

--- Assignments/t-test_2019-09-24.R
+++ Assignments/t-test_2019-09-24.R
@@ -2,8 +2,10 @@ rm(list = ls())
 getwd()
 library("tidyverse")
 tidyverse_update()
+
 #scenario 1
 data01 <- read_csv("datasets/demos/countrybirthrate.csv")
+
 #summary stats
 summ_d <- data01 %>%
   summarise(n_d = n(),
@@ -14,11 +16,39 @@ summ_d <- data01 %>%
             var_d = var(d))
 
 View(summ_d)
+
 #boxplot
 ggplot(data01)+
   geom_boxplot(aes(x = "", y = d), notch = FALSE, varwidth = TRUE)
+
+#histogram
+ggplot(data01) +
+  geom_histogram(aes(d), binwidth = 4)
 
 
 #scenario 2
 data02 <- read_csv("datasets/abd/chapter12/chap12e3HornedLizards.csv")
 data02 <- data02 %>% slice (-105)
+
+
+#summary stats
+summ_HornLength <- data02 %>%
+  summarise(n_HornLength = n(),
+            mean_HornLength = mean(squamosalHornLength),
+            median_HornLength = median(squamosalHornLength),
+            IQR_HornLength = IQR(squamosalHornLength),
+            sd_HornLength = sd(squamosalHornLength),
+            var_HornLength = var(squamosalHornLength))
+
+View(summ_HornLength)
+
+#boxplot
+ggplot(data02)+
+  geom_boxplot(aes(x = Survival, y = squamosalHornLength), notch = FALSE, varwidth = TRUE)
+
+#histogram
+ggplot(data02) +
+  geom_histogram(aes(squamosalHornLength), binwidth = 2)+
+  facet_wrap(~Survival)
+
+
