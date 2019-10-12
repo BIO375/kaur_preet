f8f04d12cc37eb857dea09b88b4ef8e5e3b05af3
Manpreet Kaur <manpreetkaur.mk1997@gmail.com>
2019-10-12 03:07:48 -0400

    final attempt

--- Assignments/2019-10-08_Lab6.R
+++ Assignments/2019-10-08_Lab6.R
@@ -42,7 +42,7 @@ ggplot(data01) +
 ggplot(data01)+
   geom_qq(aes(sample = skinColor, color = species))
 
-#Normality was met but due to high ratio, using Welch's test
+#Normality was met but due to high ratio (4.30, which is greater than 3), using Welch's test
 t.test (skinColor ~ species, data = data01, alternative = "two.sided", conf.level = 0.95)
 
 
@@ -72,7 +72,7 @@ ggplot(data01) +
 
 ggplot(data01)+
   geom_qq(aes(sample = log_skincolor, color = species))
-# less than 3 and normality met therefore can use two sample t-test
+# ratio was less than 3 (2.62) and normality met (the median was very close to the center of the IQR box, lines in qq plot were close to being straight, histogram showed a normal (bell curve) looking plot)therefore can use two sample t-test
 t.test(log_skincolor ~ species, data = data01, var.equal = TRUE, alternative = "two.sided", conf.level = 0.95)
 
 #a. list two methods that would be appropriate to test whether there was a difference in mean skin color between the two groups
@@ -94,7 +94,7 @@ ggplot(data02) +
 ggplot(data02)+
   geom_qq(aes(sample = biomassChange))
 
-#Due to normality being met, can use one sample t-test
+#Due to normality being met(boxplot - middle median line was in the middle of the IQR box, histogram and boxplot show a bit of a left skew with outliers below the box and a longer whisker below the box, also the bell shape is more the right on the histogram, qq plot is a little rounded but is still pretty straight), can use one sample t-test
 t.test(data02$biomassChange, 
        alternative = "two.sided", mu = 0, conf.level = 0.95)
 
@@ -104,16 +104,59 @@ t.test(data02$biomassChange,
 
 #chpt 13 #26 ####
 data03 <- read_csv("datasets/abd/chapter13/chap13q26ZebraFinchBeaks.csv",col_names = TRUE)
+
+#checking for assumptions of normality
+ggplot(data03) +
+  geom_histogram(aes(preference), binwidth = 10)
+
+ggplot(data03) +
+  geom_boxplot(aes(x = "", y = preference))
+
+ggplot(data03)+
+  geom_qq(aes(sample = preference))
+
+# normality not met: the histogram does not show much because of low sample size, the boxplot shows that the upper whisker is a little longer than the bottom (indicating a bit of a postive skew) and median is lower than normal but still towards the middle, the qq plot looks a bit like a sigmoid graph, will use sign test because want to be conservative as the plots were not exactly normally distributed
+# One-sided, HA that preference for male with higher cartenoid diet is higher than preference for male with low cartenoid diet for females
+SignTest(data03$preference, alternative = "greater", mu = 0, conf.level = 0.95)
+
                   
 #choose an appropriate method and test whether females preferred one type of male over the other type
-
-
+### Should use paired t-test because the same experiemental group is used for both treatments (same females used for high dietary cartenoid male and diet low in cartenoids male).
+### used sign test because normality was not met, sign test showed that females preferred to sit with the male who had a high cartenoid diet than with the male that had a low cartenoid diet (S = 10, n = 10, p = 0.0009766)
 
 
 #review 2: #16 ####
 data04 <-read_csv("datasets/abd/chapter03/chap03q22ZebraFishBoldness.csv", col_names = TRUE)
+# two sided two sample t-test
+# summarize to get sd
+summ_SAA <- data04 %>%
+  group_by(genotype) %>% 
+  summarise(n_SAA = n(),
+            mean_SAA = mean(secondsAggressiveActivity),
+            sd_SAA = sd(secondsAggressiveActivity)
+            )
 
+View(summ_SAA)
+# use sd to see what the ratio is, which tells if data meets assumption of homoscedasticity
+ratio03 <-(max(summ_SAA$sd_SAA))/(min(summ_SAA$sd_SAA))
+# see if assumption of normality is met
+ggplot(data04) +
+  geom_histogram(aes(secondsAggressiveActivity), binwidth = 40)+
+  facet_wrap(~genotype)
 
+ggplot(data04) +
+  geom_boxplot(aes(x = genotype, y = secondsAggressiveActivity))
+
+ggplot(data04)+
+  geom_qq(aes(sample = secondsAggressiveActivity, color = genotype))
+
+#ratio is less than 3, therefore variability is low; normality assumption is also met because the boxplot shows the middle median line is in the middle for spd and is a little high for wild type but still towards the middle and the qq plots both have similar shapes that look linear, therefore can use two-sample t-test
+t.test(secondsAggressiveActivity ~ genotype, data = data04, var.equal = TRUE, alternative = "two.sided", conf.level = 0.95)
+
+#a. estimate the magnitude of the effect of the mutation (diff between means) on the amount of time spent in aggressive activity. put appropriate bounds on your estimate of the effect 
+### The confidence interval is 25.93 to 110.26 which is a very broad range. 
+#b. what is the weight of evidence that this effect is not zero? (pvalue) perform appropriate stat test of diff
+### The weight of evidene that the effect is not zero is 0.003142 (the p-value). There is a difference between individuals who had the gene and those who did not in terms of how long they will aggressively act when shown a mirror (t = 3.3802, df = 19, p-value = 0.003142).
 
 
 
