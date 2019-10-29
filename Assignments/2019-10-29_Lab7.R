4d12feb39353c736b944e449c9e63bce8a0d935c
Manpreet Kaur <manpreetkaur.mk1997@gmail.com>
2019-10-29 12:56:43 -0400

    attempt 2.3

--- Assignments/2019-10-29_Lab7.R
+++ Assignments/2019-10-29_Lab7.R
@@ -3,12 +3,19 @@
 rm(list = ls())
 #verify working directory
 getwd()
-# install packages
+# install packages; ggfortify works with ggplot2 to make nice plots, multcomp does contrasts
+# and multiple comparison; nlme is used for random effects ANOVA
+install.packages("ggfortify")
+library("ggfortify")
+install.packages("multcomp")
+library("multcomp")
+install.packages("nlme")
+library("nlme")
 
+# load tidyverse and check for updates
 library("tidyverse")
 tidyverse_update()
 
 #read in dataset
 data01 <- read.csv("datasets/demos/Jaffe.csv")
-install.packages("rlang")
 
