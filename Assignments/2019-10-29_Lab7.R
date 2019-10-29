375fbd0a029adefc2235758b908bf917e24ea58e
Manpreet Kaur <manpreetkaur.mk1997@gmail.com>
2019-10-29 12:52:34 -0400

    attempt 2.2

--- Assignments/2019-10-29_Lab7.R
+++ Assignments/2019-10-29_Lab7.R
@@ -1,9 +1,14 @@
+# one way ANOVA lab
+#clean up working environment
 rm(list = ls())
+#verify working directory
 getwd()
-install.packages("tidyverse") 
+# install packages
+
 library("tidyverse")
 tidyverse_update()
 
 #read in dataset
 data01 <- read.csv("datasets/demos/Jaffe.csv")
+install.packages("rlang")
 
