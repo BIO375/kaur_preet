aaa9ed471e399e738b836660f69f8db9eee11495
Manpreet Kaur <manpreetkaur.mk1997@gmail.com>
2019-11-05 15:00:39 -0500

    attempt two

--- Assignments/2019-11-05_Lab8.R
+++ Assignments/2019-11-05_Lab8.R
@@ -36,10 +36,12 @@ varWithin01 <- as.numeric( model01_varcomp[2,1] )
 repeatability <- varAmong01 / (varAmong01 + varWithin01)
 repeatability
 #the repeatability is 0.5970059
-############NEED TO DO PART D ##########
+#The repeatability for example 15.6 is 0.75, therefore in comparison to this data, example 15.6 
+#has higher repeatability and is less affected by measurement error.
 
 #### Ch 15 Problem 23, Complete part a only ####
-data02 <- read_csv("datasets/abd/chapter15/chap15q23LodgepolePineCones.csv")
+data02 <- read_csv("datasets/abd/chapter15/chap15q23LodgepolePineCones.csv", col_types = cols(
+  habitat = col_factor()))
 
 ggplot(data02, aes(x = habitat, y = conemass))+
   geom_boxplot() +
@@ -53,15 +55,41 @@ ggplot(data02)+
 
 model02 <- lm(conemass~habitat, data = data02)
 
+summ_conemass <- data02 %>%
+  group_by(habitat) %>% 
+  summarise(mean_conemass = mean(conemass),
+            sd_conemass = sd(conemass),
+            n_conemass = n())
+ratio02 <-(max(summ_conemass$sd_conemass))/(min(summ_conemass$sd_conemass))
+
+autoplot(model02)
+
+anova(model02)
+
+summary(model02)
 
 #this comparison is a planned comparison
 planned02 <- glht(model02, linfct = 
                   mcp(habitat = c("island.present - island.absent = 0")))
-confint(planned)
-summary(planned)
+confint(planned02)
+summary(planned02)
+# The two groups, squirrels on islands and squirrels not on islands are significantly different 
+#(planned comparison, t=-8.596, p<0.001)
+
 
 #### Ch 15 Problem 26, Use data to perform the correct test, show code for all steps in your process ####
-data03 <- read_csv("datasets/abd/chapter15/chap15q26MalariaFungusVenom.csv")
+data03 <- read_csv("datasets/abd/chapter15/chap15q26MalariaFungusVenom.csv", col_types = cols(
+  treatmentGroup = col_factor()))
+
+
+
 
 #### Ch 15 Problem 30, Use data to perform the correct test, show code for all steps in your process ####
-data04 <- read_csv("datasets/abd/chapter15/chap15q30FiddlerCrabFans.csv")
+data04 <- read_csv("datasets/abd/chapter15/chap15q30FiddlerCrabFans.csv", col_types = cols(
+  crabType = col_factor()))
+data04 <- slice(data04, -85)
+
+
+
+
+
