206eee7e8ad4d6595a00c12d7c4550b986e113e1
Manpreet Kaur <manpreetkaur.mk1997@gmail.com>
2019-11-26 13:26:55 -0500

    attempt 1

---
+++ Assignments/2019-11-26_Lab10.R
@@ -0,0 +1,11 @@
+# chi squared and friends ####
+rm(list = ls())
+getwd()
+
+# installing the new packages and loading tidyverse
+install.packages("ggmosaic")
+library("ggmosaic")
+install.packages("epitools")
+library("epitools")
+library("tidyverse")
+tidyverse_update()
